a2f1152f1901a04a98f329a9d8254a018b5d5cfc
kshetragyacybersec-lang <kshetragyacybersec@gmail.com>
2026-08-07 15:14:51 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero.jsx';
+import Process from '../components/Process.jsx';
+import Contact from '../components/Contact.jsx';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Process />
+      <Contact />
+    </>
+  );
+}
