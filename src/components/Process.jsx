b2d4859339794cda45f53e7232959f99d72e06a8
kshetragyacybersec-lang <kshetragyacybersec@gmail.com>
2026-08-08 14:50:39 +0530

    Update Process.jsx

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { processSteps } from '../data.js';
 import { useScrollReveal } from '../useScrollReveal.js';
 
