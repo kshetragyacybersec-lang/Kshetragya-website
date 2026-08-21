e0f87396e03c7c06968eefd0b64b32ed56f49753
kshetragyacybersec-lang <kshetragyacybersec@gmail.com>
2026-08-21 16:02:08 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import Home from './pages/Home.jsx';
 import ServiceDetail from './pages/ServiceDetail.jsx';
 import About from './pages/About.jsx';
 import Careers from './pages/Careers.jsx';
+import Blog from './pages/Blog.jsx';
+import BlogPost from './pages/BlogPost.jsx';
+import CaseStudies from './pages/CaseStudies.jsx';
+import CaseStudyDetail from './pages/CaseStudyDetail.jsx';
 import NotFound from './pages/NotFound.jsx';
+import AdminApp from './admin/AdminApp.jsx';
 
-export default function App() {
+function PublicSite() {
   return (
     <>
       <Nav />
@@ -16,9 +21,22 @@ export default function App() {
         <Route path="/services/:slug" element={<ServiceDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="/careers" element={<Careers />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="/case-studies" element={<CaseStudies />} />
+        <Route path="/case-studies/:slug" element={<CaseStudyDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/admin/*" element={<AdminApp />} />
+      <Route path="/*" element={<PublicSite />} />
+    </Routes>
   );
 }
